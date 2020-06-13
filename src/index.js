b3e484ac1451cdc8161da2e2ca439cd4fecbe741
RickyC0626 <rickychon99@gmail.com>
2020-06-13 19:29:08 -0400

    Switch to BrowserRouter due to routing issues

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './view/App/App';
 import store from './state/shared/store/store';
 import { Provider } from 'react-redux';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
