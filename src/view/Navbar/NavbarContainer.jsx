0ced7ce3e6a30b9f8894353fe2b3a33e46dbc00f
RickyC0626 <rickychon99@gmail.com>
2020-06-13 21:11:39 -0400

    Add navbar components

---
+++ src/view/Navbar/NavbarContainer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import { NavbarView } from "../Navbar";
+
+class NavbarContainer extends Component
+{
+    render()
+    {
+        return <NavbarView />;
+    }
+}
+
+export default NavbarContainer;
---
+++ src/view/Navbar/index.js
@@ -0,0 +1,7 @@
+// Barrel file for exporting all Navbar containers and views
+
+// CONTAINERS
+export { default as NavbarContainer } from "./NavbarContainer";
+
+// VIEWS
+export { default as NavbarView } from "./NavbarView";
