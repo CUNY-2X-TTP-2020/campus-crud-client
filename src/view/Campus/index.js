5851432438b6f5313a6b5aec8860577ddf4c8055
RickyC0626 <rickychon99@gmail.com>
2020-06-14 01:47:01 -0400

    Add container and view for single campus, along with routes

--- src/view/Campus/index.js
+++ src/view/Campus/index.js
@@ -2,6 +2,8 @@
 
 // CONTAINERS
 export { default as AllCampusesContainer } from "./AllCampusesContainer";
+export { default as CampusContainer } from "./CampusContainer";
 
 // VIEWS
 export { default as AllCampusesView } from "./AllCampusesView";
+export { default as CampusView } from "./CampusView";
--- src/view/Routes/RoutesView.jsx
+++ src/view/Routes/RoutesView.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import { AllCampusesContainer } from "../Campus";
+import { AllCampusesContainer, CampusContainer } from "../Campus";
 import { AllStudentsContainer } from "../Student";
 
 const RoutesView = () =>
@@ -10,6 +10,7 @@ const RoutesView = () =>
         <Switch>
             <Route exact path="/" component={AllCampusesContainer} />
             <Route exact path="/campuses" component={AllCampusesContainer} />
+            <Route exact path="/campuses/:id" component={CampusContainer} />
             <Route exact path="/students" component={AllStudentsContainer} />
         </Switch>
     );
